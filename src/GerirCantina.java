d112e52ccd5fff397a078b64e772fe5dd158a631
Guilherme Graça <gui.gomes.graca@email.com>
2026-05-18 10:20:54 +0100

    Metodos de carregar e ler dados
    Signed-off-by: Guilherme Graça <gui.gomes.graca@email.com>

--- src/GerirCantina.java
+++ src/GerirCantina.java
@@ -1,6 +1,17 @@
 import java.util.ArrayList;
+import java.io.*;
 
 public class GerirCantina {
+
+    private static GerirCantina instance;
+
+    public static GerirCantina getInstance(){
+        if(instance == null){
+            instance = new GerirCantina();
+        }
+        return instance;
+    }
+
     private ArrayList<Utilizador> utilizadores;
     private Cantina cantina;
 
@@ -8,4 +19,42 @@ public class GerirCantina {
         this.utilizadores = new ArrayList<>();
         this.cantina = new Cantina();
     }
+
+
+
+
+
+
+    /**
+     * Metodo para guardar os dados no ficheiro ("dados.dat") sempre que o projeto fecha
+     */
+    public void guardarDados(){
+        try{
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("dados.dat"));
+
+            out.writeObject(utilizadores);
+            out.writeObject(cantina);
+
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Metodo para caregar os dados do ficheiro ("dados.dat")
+     */
+    public void carregarDados(){
+        try {
+            ObjectInputStream in = new ObjectInputStream( new FileInputStream("dados.dat"));
+
+            utilizadores = (ArrayList<Utilizador>)  in.readObject();
+            cantina = (Cantina) in.readObject();
+
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
